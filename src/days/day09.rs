23b779fcd4b48572d7074fc76865d91627367aa2
Robbe Van Herck <robbe@robbevanherck.be>
2020-12-09 11:15:56 +0100

    Solution for day 9

---
+++ src/days/day09.rs
@@ -0,0 +1,56 @@
+use crate::day::Day;
+
+use itertools::iproduct;
+
+pub struct Day09 {}
+
+impl Day for Day09 {
+    type InputElement = usize;
+    type Output1 = usize;
+    type Output2 = usize;
+
+    fn solve_part1(&self, input: &Vec<Self::InputElement>) -> Self::Output1 {
+        let preamble_count = 25;
+        *input.windows(preamble_count + 1).map(|slice| {
+            let target: &usize = slice.last().expect("Unreachable statement");
+            (iproduct!(slice, slice)
+                .filter(move |(x, y)| x != y && *x + *y == *target)
+                .map(|(x, y)| x + y)
+                .next(), target)
+        }).filter(|(v,_)| *v == None)
+          .next()
+          .expect("No solution")
+          .1
+    }
+
+    fn solve_part2(&self, input: &Vec<Self::InputElement>) -> Self::Output2 {
+        let target = self.solve_part1(input);
+        let mut curr_sum = 0;
+        let mut start = 0;
+        let mut end = 0;
+        for (i, last) in input.iter().enumerate() {
+            curr_sum += last;
+            end = i;
+            while curr_sum > target {
+                curr_sum -= input[start];
+                start += 1;
+            }
+            if curr_sum == target {
+                break;
+            }
+        }
+        let (min, max) = &input[start..=end].iter()
+                            .fold((usize::MAX, usize::MIN),
+                                    |(c_min, c_max), val| (c_min.min(*val), c_max.max(*val)));
+        min + max
+
+        
+    }
+
+    fn parse_input(&self, content: String) -> Vec<Self::InputElement> {
+        content.lines().map(|x|
+                        x.parse::<Self::InputElement>()
+                        .expect("Invalid input")
+                ).collect()
+    }
+}
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -6,6 +6,7 @@ mod day05;
 mod day06;
 mod day07;
 mod day08;
+mod day09;
 
 use crate::day::Day;
 use std::path::PathBuf;
@@ -13,11 +14,11 @@ use std::path::PathBuf;
 pub fn run_day(day_num: usize, bench_num: usize, part_num: Option<usize>, input_file: Option<PathBuf>, timed: bool) {
     let run_part1 = match part_num {
         None => true,
-        Some(x) => x == 1
+        Some(x) => x == 1 || x == 0
     };
     let run_part2 = match part_num {
         None => true,
-        Some(x) => x == 2
+        Some(x) => x == 2 || x == 0
     };
     match day_num {
         1 => day01::Day01{}.execute(day_num, bench_num, run_part1, run_part2, input_file, timed),
@@ -28,6 +29,7 @@ pub fn run_day(day_num: usize, bench_num: usize, part_num: Option<usize>, input_
         6 => day06::Day06{}.execute(day_num, bench_num, run_part1, run_part2, input_file, timed),
         7 => day07::Day07{}.execute(day_num, bench_num, run_part1, run_part2, input_file, timed),
         8 => day08::Day08{}.execute(day_num, bench_num, run_part1, run_part2, input_file, timed),
+        9 => day09::Day09{}.execute(day_num, bench_num, run_part1, run_part2, input_file, timed),
         _ => panic!("Day {} hasn't been solved (yet)", day_num),
     };
 }
