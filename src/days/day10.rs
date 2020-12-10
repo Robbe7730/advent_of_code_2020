fbaee397922abc3d125e847df4f90ad235ca05cb
Robbe Van Herck <robbe@robbevanherck.be>
2020-12-10 09:57:56 +0100

    Solution for day 10

---
+++ src/days/day10.rs
@@ -0,0 +1,46 @@
+use crate::day::Day;
+
+pub struct Day10 {}
+
+impl Day for Day10 {
+    type InputElement = usize;
+    type Output1 = usize;
+    type Output2 = usize;
+
+    fn solve_part1(&self, old_input: &Vec<Self::InputElement>) -> Self::Output1 {
+        let mut input = old_input.to_vec();
+        input.sort();
+        input.insert(0, 0);
+        let (one, _two, three) = input.windows(2)
+             .map(|win| win[1] - win[0])
+             .fold((0,0,0), |(one,two,three), diff|
+                   (one + (diff == 1) as usize,
+                    two + (diff == 2) as usize,
+                    three + (diff == 3) as usize));
+        one * (three+1)
+    }
+
+    fn solve_part2(&self, old_input: &Vec<Self::InputElement>) -> Self::Output2 {
+        let mut input = old_input.to_vec();
+        input.sort();
+        input.insert(0, 0);
+        let (_, _, ret) = input.windows(2)
+             .map(|win| win[1] - win[0])
+             .fold((0,0,1), |(three,two,one), diff| {
+                 match diff {
+                    1 => (two, one, one+two+three),
+                    2 => (one, 0, one+two),
+                    3 => (0, 0, one),
+                    _ => (0, 0, 0)
+                 }
+             });
+        ret
+    }
+
+    fn parse_input(&self, content: String) -> Vec<Self::InputElement> {
+        content.lines().map(|x|
+                        x.parse::<Self::InputElement>()
+                        .expect("Invalid input")
+                ).collect()
+    }
+}
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -7,6 +7,7 @@ mod day06;
 mod day07;
 mod day08;
 mod day09;
+mod day10;
 
 use crate::day::Day;
 use std::path::PathBuf;
@@ -30,6 +31,7 @@ pub fn run_day(day_num: usize, bench_num: usize, part_num: Option<usize>, input_
         7 => day07::Day07{}.execute(day_num, bench_num, run_part1, run_part2, input_file, timed),
         8 => day08::Day08{}.execute(day_num, bench_num, run_part1, run_part2, input_file, timed),
         9 => day09::Day09{}.execute(day_num, bench_num, run_part1, run_part2, input_file, timed),
+        10 => day10::Day10{}.execute(day_num, bench_num, run_part1, run_part2, input_file, timed),
         _ => panic!("Day {} hasn't been solved (yet)", day_num),
     };
 }
